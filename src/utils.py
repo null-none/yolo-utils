5fac367c4d082ebffbcd5f8b4b779bf3f174ca89
Dmitry Kalinin <kalinin.mitko@gmail.com>
2025-01-02 11:00:56 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,71 @@ class YoloUtils:
         self.image_width = 1280
         self.image_height = 720
 
+    def non_negative(self, coord):
+        
+        """
+            Sets negative coordinates to zero. This fixes bugs in some labeling tools.
+            
+            Input:
+                coord: Int or float
+                Any number that represents a coordinate, whether normalized or not.
+        """
+        
+        if coord < 0:
+            return 0
+        else:
+            return coord
+        
+    def pixel2yolo(self, dim, pixel_coords):
+        
+        """
+            Transforms coordinates in YOLO format to coordinates in pixels.
+            
+            Input:
+                dim: Tuple or list
+                Image size (width, height).
+                pixel_coords: List
+                Bounding box coordinates in pixels (xmin, ymin, xmax, ymax).
+            Output:
+                yolo_coords: List
+                Bounding box coordinates in YOLO format (xcenter, ycenter, width, height).
+        """
+        
+        dw = 1/dim[0]
+        dh = 1/dim[1]
+        xcenter = non_negative(dw*(pixel_coords[0] + pixel_coords[2])/2)
+        ycenter = non_negative(dh*(pixel_coords[1] + pixel_coords[3])/2)
+        width = non_negative(dw*(pixel_coords[2] - pixel_coords[0]))
+        height = non_negative(dh*(pixel_coords[3] - pixel_coords[1]))
+        
+        yolo_coords = [xcenter, ycenter, width, height]
+        
+        return yolo_coords
+    
+    def yolo2pixel(self, dim, yolo_coords):
+        
+        """
+            Transforms coordinates in YOLO format to coordinates in pixels.
+            
+            Input:
+                dim: Tuple or list
+                Image size (width, height).
+                yolo_coords: List
+                Bounding box coordinates in YOLO format (xcenter, ycenter, width, height).
+            Output:
+                pixel_coords: List
+                Bounding box coordinates in pixels (xmin, ymin, xmax, ymax).
+        """
+        
+        xmin = non_negative(round(dim[0] * (yolo_coords[0] - yolo_coords[2]/2)))
+        xmax = non_negative(round(dim[0] * (yolo_coords[0] + yolo_coords[2]/2)))
+        ymin = non_negative(round(dim[1] * (yolo_coords[1] - yolo_coords[3]/2)))
+        ymax = non_negative(round(dim[1] * (yolo_coords[1] + yolo_coords[3]/2)))
+        
+        pixel_coords = [xmin, ymin, xmax, ymax]
+        
+        return pixel_coords
+    
     def seg_to_bbox(self, seg_info):
         # Example input: 5 0.046875 0.369141 0.0644531 0.384766 0.0800781 0.402344 ...
         class_id, *points = seg_info.split()
